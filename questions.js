7d43a9bd17cab8703bc2bff1d164b001036c29ae
Tyler Hoff <tylerjahoff@gmail.com>
2018-05-29 16:53:02 -0400

    moved functions

--- questions.js
+++ questions.js
@@ -17,6 +17,27 @@ function resetQuestions() {
     location.reload(true);
 }
 
+function createCookie(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        expires = "; expires="+date.toGMTString();
+    }
+    else expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
 function initializeQuestions() {
     // Newsletter Cookie DOES NOT EXIST so show Newsletter question
     if ( readCookie(nCookie)==null ) {
@@ -149,27 +170,6 @@ function logAnswer(qnumber, snippet, answer) {
                 //alert(data);
                 }
                 });
-}
-
-function createCookie(name,value,days) {
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime()+(days*24*60*60*1000));
-        expires = "; expires="+date.toGMTString();
-    }
-    else expires = "";
-    document.cookie = name+"="+value+expires+"; path=/";
-}
-
-function readCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
 }
 
 function eraseCookie(name) { createCookie(name,"",-1); }
